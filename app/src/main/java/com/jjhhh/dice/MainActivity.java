d30efe7efee1d175ec44fb0a51b6d20e25781f0e
Helen Zhao <helen4251996@yahoo.co.nz>
2017-03-29 16:07:49 +1300

    Add basic rolling for six sided dice

--- app/src/main/java/com/jjhhh/dice/MainActivity.java
+++ app/src/main/java/com/jjhhh/dice/MainActivity.java
@@ -2,6 +2,8 @@ package com.jjhhh.dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void rollDice(View view) {
+        TextView tv = (TextView) findViewById(R.id.rollNumber);
+        tv.setText(Integer.toString(getDiceRollSix()));
+    }
+
+    private int getDiceRollSix() {
+        return (int)(Math.floor((Math.random() * 6)) + 1);
     }
 }
